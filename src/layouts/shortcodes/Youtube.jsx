c5c61fdb14d0b22f742997b5bc1d3f0cea5ac7b0
jhonnjc15 <59791854+jhonnjc15@users.noreply.github.com>
2026-01-25 21:05:39 -0500

    Replace YouTube embed with iframe

--- src/layouts/shortcodes/Youtube.jsx
+++ src/layouts/shortcodes/Youtube.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-
-import LiteYouTubeEmbed from "react-lite-youtube-embed";
-import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
+import React from "react";
 
 const Youtube = ({ id, title, ...rest }) => {
   return (
-    <LiteYouTubeEmbed
-      id={id}
-      title={title}
-      {...rest}
-      wrapperClass="yt-lite rounded-md"
-    />
+    <div className="relative w-full overflow-hidden rounded-md pb-[56.25%] bg-black/5">
+      <iframe
+        className="absolute inset-0 h-full w-full"
+        src={`https://www.youtube.com/embed/${id}`}
+        title={title}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        {...rest}
+      />
+    </div>
   );
 };
 
